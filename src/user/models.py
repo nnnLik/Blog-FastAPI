2cc2fc4fdfbf85fb21ee3b7c25c50b020e178e77
nnnLik <makhmudov_ra@vk.com>
2023-01-29 20:33:23 +0300

    [ADD] profile model

--- src/user/models.py
+++ src/user/models.py
@@ -1,7 +1,17 @@
 from core.database import Base
 
-from sqlalchemy import Column, String, Integer, DateTime, Boolean
+from sqlalchemy import (
+    Column, String, Integer,
+    DateTime, Boolean, Text,
+    ForeignKey, Enum)
 
+
+class UserGenders(Enum):
+    """User Genders choice type class"""
+
+    male = 'male'
+    female = 'female'
+    other = 'other'
 
 class User(Base):
     """User table model"""
@@ -20,3 +30,29 @@ class User(Base):
 
     is_admin = Column(Boolean, default=False)
     is_active = Column(Boolean, default=False)
+
+
+class Profile(Base):
+    """User table model"""
+
+    __tablename__ = 'blog_profile'
+
+    id = Column(Integer, primary_key=True, index=True, unique=True)
+
+    first_name = Column(String)
+    second_name = Column(String)
+
+    sex = Column(Enum(UserGenders))
+
+    birthdate = Column(DateTime)
+    biography = Column(Text)
+
+    github = Column(String)
+    linkedin = Column(String)
+    twitter = Column(String)
+    facebook = Column(String)
+
+    favourite = Column(Integer, ForeignKey('blog_post.id'))
+
+
+
